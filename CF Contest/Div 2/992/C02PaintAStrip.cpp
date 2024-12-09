ba9bcde265e9206ae84e878890617531866310b1
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-12-09 17:53:14 +0530

    Contest-4 Div 2 992 QB Paint a strip : Tried solved it using manipulating the formula. But ended up doing 3 wrong submission and I still dont know why the solution did not work. Then saw the tutorial and tried it using different way and felt explanation of 1 more test case would have given a better idea to me.

---
+++ CF Contest/Div 2/992/C02PaintAStrip.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        ll n;
+        cin >> n;
+
+        vector<ll> cnt(1e5 + 1);
+        cnt[1] = 1;
+
+        ll x = 1;
+        ll j = 2;
+        for (ll i = 2; i < 1e9; i++)
+        {
+            x = x * 2 + 2;
+            while (j <= x && j < 1e5)
+            {
+                cnt[j] = i;
+                j++;
+            }
+            if (x > 1e5)
+            {
+                break;
+            }
+        }
+
+        cout << cnt[n] << endl;
+    }
+
+    return 0;
+}
