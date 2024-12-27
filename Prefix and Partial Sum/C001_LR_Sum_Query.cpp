6286f01c893e59c6c07ae54fa72f1cd42165f6c5
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-12-28 04:59:45 +0530

    LR Sum Query : Question was related to prefix sum. Just had to print sum of ranges but the trick was by doing mod and I really  need to work of Mod Arith a bit. I did not consider thee possibility of getting negative values on subtracting after doing MOD and ended up not add MOD to it. But then asked AI and i realised the mistake I made.

---
+++ Prefix and Partial Sum/C001_LR_Sum_Query.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n, q;
+    cin >> n >> q;
+
+    vll nums(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    vll ps(n);
+    ps[0] = nums[0] % MOD + MOD;
+    for (ll i = 1; i < n; i++)
+    {
+        ps[i] = (nums[i] % MOD + MOD + ps[i - 1]) % MOD;
+    }
+
+    while (q--)
+    {
+        ll s, e;
+        cin >> s >> e;
+
+        s == 1 ? cout << (ps[e - 1] - 0 + MOD) % MOD << endl : cout << (ps[e - 1] - ps[s - 2] + MOD) % MOD << endl;
+    }
+
+    return 0;
+}
