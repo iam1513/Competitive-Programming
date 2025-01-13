d41b62d33bbda01ab6cdb5e654dd2645cdc0276c
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-01-13 16:47:11 +0530

    2051C - Preparing for the Exam : Tried this during the contest. Got accepted during the contest and then got unaccepted after the contest. The solution was too messy and that i relealized after seeing the Tutorial solution which was sweet and simple. Have to think of writing small and effective solution rather than rushing and writing a big pile of Code.

---
+++ Random CF/C009_D3_995_C.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, m, k;
+        cin >> n >> m >> k;
+
+        vll nums(m), qs(k);
+        vector<bool> check(n + 1, 0);
+
+        for (ll i = 0; i < m; i++)
+        {
+            cin >> nums[i];
+        }
+
+        for (ll i = 0; i < k; i++)
+        {
+            cin >> qs[i];
+            check[qs[i]] = true;
+        }
+
+        for (ll i = 0; i < m; i++)
+        {
+            if (k == n || (k == n - 1 && !check[nums[i]]))
+            {
+                cout << 1;
+            }
+            else
+            {
+                cout << 0;
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
