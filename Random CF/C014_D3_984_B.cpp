7c03f702774b2231482e29127f3ac96e52ad2562
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-01-18 15:47:28 +0530

    Random CF : Was easy 800 rated problem.

---
+++ Random CF/C014_D3_984_B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, k;
+        cin >> n >> k;
+        vll nums(k + 1, 0);
+
+        for (ll i = 0; i < k; i++)
+        {
+            ll b, c;
+            cin >> b >> c;
+            nums[b] += c;
+        }
+
+        sort(nums.rbegin(), nums.rend());
+        ll ans = 0;
+        for (ll i = 0; i < min(n, k); i++)
+        {
+            ans += nums[i];
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
