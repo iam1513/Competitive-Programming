a6c8b0d2daa10059f1d0ed4bd05d2ca5f8259a70
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-02-13 15:51:18 +0530

    CP 31 : Rated 1000 : qs 013 Luke Is A Foodie : Tried it using sorting but was wrong. The intuition was keep finding whether the gap between two numbers is in range of 2x. If the gap exceeds that , we need to insert a number. The whole qs was ased on greedy intuition. I tried to do the same but tried it using sorting.

--- CF Rated 1000/C013LukeIsAFoodie.cpp
+++ CF Rated 1000/C013LukeIsAFoodie.cpp
@@ -1,5 +1,36 @@
 #include <bits/stdc++.h>
+
 #define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
 using namespace std;
 
 int main()
@@ -12,6 +43,35 @@ int main()
 
     while (t--)
     {
+        ll n, x;
+        cin >> n >> x;
+
+        vll nums(n + 1);
+        for (ll i = 1; i <= n; i++)
+        {
+            cin >> nums[i];
+        }
+
+        ll num_min = nums[1];
+        ll num_max = nums[1];
+        ll res = 0;
+        for (ll i = 2; i <= n; i++)
+        {
+            if (nums[i] > num_max)
+            {
+                num_max = nums[i];
+            }
+            if (nums[i] < num_min)
+            {
+                num_min = nums[i];
+            }
+            if (num_max - num_min > 2 * x)
+            {
+                res++;
+                num_min = num_max = nums[i];
+            }
+        }
+        cout << res << endl;
     }
 
     return 0;
