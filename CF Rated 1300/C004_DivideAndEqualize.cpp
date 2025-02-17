d38c89c0881c50ee2128067338e3405ed0f63c52
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-02-17 20:15:25 +0530

    CP 31 : Rated 1300 : qs 04 Divide and Equalize : Was not able to get it at all. The crux was if array size is 5 and one divisor is 2, then number of times 2 occurs as divisor including all the numbers divisors and this is case for each divisor, then nd then only its possible to do this stuff, else not.

---
+++ CF Rated 1300/C004_DivideAndEqualize.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+void get_divisors(ll n, mll &mp)
+{
+
+    ll i = 2;
+    while (i * i <= n)
+    {
+        while (n % i == 0)
+        {
+            mp[i]++;
+            n /= i;
+        }
+        i++;
+    }
+    if (n > 1)
+    {
+        mp[n]++;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vll nums(n);
+        mll mp;
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> nums[i];
+            get_divisors(nums[i], mp);
+        }
+
+        bool flag = false;
+        for (auto ele : mp)
+        {
+            if (ele.second % n != 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+
+    return 0;
+}
