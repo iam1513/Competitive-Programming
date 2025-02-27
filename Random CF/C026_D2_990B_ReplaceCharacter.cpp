434c5b69ce9a502a33a46ec39390e8be5385d793
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-02-27 12:44:04 +0530

    Random CF : Simple 900 rated qs.

---
+++ Random CF/C026_D2_990B_ReplaceCharacter.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        string st;
+        cin >> st;
+
+        vll nums(26, 0);
+
+        for (ll i = 0; i < n; i++)
+        {
+            nums[st[i] - 'a']++;
+        }
+        ll maxi = INT_MIN;
+        ll mini = INT_MAX;
+        char ch1, ch2;
+
+        for (ll i = 0; i < 26; i++)
+        {
+            if (nums[i] >= maxi)
+            {
+                ch1 = i + 'a';
+                maxi = nums[i];
+            }
+            if (nums[i] < mini && nums[i] != 0)
+            {
+                ch2 = i + 'a';
+                mini = nums[i];
+            }
+        }
+
+        for (ll i = 0; i < n; i++)
+        {
+            if (st[i] == ch2)
+            {
+                st[i] = ch1;
+                break;
+            }
+        }
+
+        cout << st << endl;
+    }
+
+    return 0;
+}
