4dd8333a640fc9f3ef4b850d665648529f73e5d9
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-03-01 08:58:03 +0530

    Random CF : Simple if-else problem.

---
+++ Random CF/C029_D2_942B_CoinGames.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        string st;
+        cin >> st;
+        ll ucnt = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            if (st[i] == 'U')
+            {
+                ucnt++;
+            }
+        }
+
+        if (ucnt % 2 == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+
+    return 0;
+}
