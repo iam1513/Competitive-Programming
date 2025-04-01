7b196752155b6a3716968d67f1a256d579a7ee8a
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-04-01 08:59:45 +0530

    CSES DP : The trick here is like in last question , we found out the minimum , we just need to add. We needed to calculate all the ways in which the sum can be produced so instead of just finding the minimum values , we added  all the ways to get to this phase.

--- CSES DP/C003CoinCombination1.cpp
+++ CSES DP/C003CoinCombination1.cpp
@@ -1,49 +1,153 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-vector<int> dp;
-const int MOD = 1e9 + 7;
-int recom(int n, int sum, vector<int> &coins)
-{
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// vector<int> dp;
+// const int MOD = 1e9 + 7;
+// int recom(int n, int sum, vector<int> &coins)
+// {
 
-    if (sum == 0)
+//     if (sum == 0)
+//     {
+//         return 1;
+//     }
+
+//     if (dp[sum] != -1)
+//     {
+//         return dp[sum];
+//     }
+
+//     int ans = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (sum - coins[i] >= 0)
+//         {
+//             ans = (ans + recom(n, sum - coins[i], coins)) % MOD;
+//         }
+//     }
+
+//     return dp[sum] = ans;
+// }
+
+// int main()
+// {
+
+//     int n, x;
+//     cin >> n >> x;
+
+//     vector<int> coins(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> coins[i];
+//     }
+
+//     dp.clear();
+//     dp.resize(x + 5, -1);
+
+//     cout << recom(n, x, coins);
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+ll f(ll n, ll x, vll &coins, vll &dp)
+{
+    if (x == 0)
     {
         return 1;
     }
 
-    if (dp[sum] != -1)
+    if (x < 0)
     {
-        return dp[sum];
+        return 0;
     }
 
-    int ans = 0;
-    for (int i = 0; i < n; i++)
+    if (dp[x] != -1)
     {
-        if (sum - coins[i] >= 0)
+        return dp[x];
+    }
+
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (x - coins[i] >= 0)
         {
-            ans = (ans + recom(n, sum - coins[i], coins)) % MOD;
+            ans = (ans + f(n, x - coins[i], coins, dp)) % MOD;
         }
     }
 
-    return dp[sum] = ans;
+    return dp[x] = ans;
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    int n, x;
+    ll n, x;
     cin >> n >> x;
 
-    vector<int> coins(n);
-    for (int i = 0; i < n; i++)
+    vll coins(n);
+    for (ll i = 0; i < n; i++)
     {
         cin >> coins[i];
     }
 
-    dp.clear();
-    dp.resize(x + 5, -1);
+    vll dp(x + 1, 0);
 
-    cout << recom(n, x, coins);
+    // ll ans = f(n, x, coins, dp);
+
+    // cout << ans << endl;
+
+
+    // TABULATION
+    dp[0] = 1;
+
+    for (ll i = 1; i <= x; i++)
+    {
+        for (ll j = 0; j < n; j++)
+        {
+            if (i - coins[j] >= 0)
+            {
+                dp[i] = (dp[i] + dp[i - coins[j]]) % MOD;
+            }
+        }
+    }
+
+    cout << dp[x] << endl;
 
     return 0;
 }
