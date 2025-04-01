d462d0dac5517e51d7eba46bc18579aa785a934c
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-04-01 11:09:00 +0530

    Random CF rated 1200 : Very easy question. Did not feel like it was 1200 at all. Now I regret not giving the previous div 3 :<.

---
+++ Random CF Rated 1200/C011_PlaceOfTheOlympiad.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, m, k;
+        cin >> n >> m >> k;
+
+        ll maxi = (k % n == 0) ? k / n : k / n + 1;
+
+        ll x = m - maxi + 1;
+
+        ll ans = (maxi % x == 0) ? maxi / x : maxi / x + 1;
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
