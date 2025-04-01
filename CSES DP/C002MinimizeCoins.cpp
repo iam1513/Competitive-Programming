a0a3828e8622e15ef5a588dc281ca9b2a0c1f1de
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-04-01 08:43:49 +0530

    CSES DP : Added Memoization and Tabulation code. Already have solved it before but did again for revision. Intuition : Say we have to make sum 11 and we have coins of 1,5,7 and say we have selected coin of 1. Now we have to make 10. If we have number of ways to make 10 , then we will just add ways in which its possible to make sum 10 and 1 as we have selected 1 so that 10+1 = 11. We can say similar thing for say 9. Then will take 1 so now we need to find for 8, then again 1 and now we need 7 and we already have 7 value coin. Therefore , 1+1+1(7 value) makes it possible to make 9 in 3 minimal coins.

--- CSES DP/C002MinimizeCoins.cpp
+++ CSES DP/C002MinimizeCoins.cpp
@@ -1,54 +1,167 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-vector<int> dp;
-int recom(int n, int sum, vector<int> &coins)
-{
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// vector<int> dp;
+// int recom(int n, int sum, vector<int> &coins)
+// {
 
-    if (sum == 0)
+//     if (sum == 0)
+//     {
+//         return 0;
+//     }
+
+//     if (dp[sum] != -1)
+//     {
+//         return dp[sum];
+//     }
+
+//     int ans = 1e9;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (sum - coins[i] >= 0)
+//         {
+//             ans = min(ans, 1 + recom(n, sum - coins[i], coins));
+//         }
+//     }
+
+//     return dp[sum] = ans;
+// }
+
+// int main()
+// {
+
+//     int n, x;
+//     cin >> n >> x;
+
+//     vector<int> coins(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> coins[i];
+//     }
+
+//     dp.clear();
+//     dp.resize(x + 5, -1);
+
+//     if (recom(n, x, coins) == 1e9)
+//     {
+//         cout << -1;
+//         return 0;
+//     }
+
+//     cout << recom(n, x, coins);
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+ll recom(ll n, ll x, vll &coins, vll &dp)
+{
+    if (x <= 0)
     {
         return 0;
     }
 
-    if (dp[sum] != -1)
+    if (dp[x] != -1)
     {
-        return dp[sum];
+        return dp[x];
     }
 
-    int ans = 1e9;
-    for (int i = 0; i < n; i++)
+    ll ans = 1e9;
+
+    for (ll i = 0; i < n; i++)
     {
-        if (sum - coins[i] >= 0)
+        if (x - coins[i] >= 0)
         {
-            ans = min(ans, 1 + recom(n, sum - coins[i], coins));
+            ans = min(ans, 1 + recom(n, x - coins[i], coins, dp));
         }
     }
 
-    return dp[sum] = ans;
+    return dp[x] = ans;
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    int n, x;
+    ll n, x;
     cin >> n >> x;
 
-    vector<int> coins(n);
-    for (int i = 0; i < n; i++)
+    // vll dp(x + 1, -1);
+    vll coins;
+    for (ll i = 0; i < n; i++)
     {
-        cin >> coins[i];
+        ll a;
+        cin >> a;
+        coins.pb(a);
     }
 
-    dp.clear();
-    dp.resize(x + 5, -1);
+    // ll ans = recom(n, x, coins, dp);
 
-    if (recom(n, x, coins) == 1e9)
+    // if (ans == 1e9)
+    // {
+    //     cout << -1 << endl;
+    // }
+    // else
+    // {
+    //     cout << ans << endl;
+    // }
+
+    // Tabulation
+    vll dp(x + 1, 1e9);
+    dp[0] = 0;
+
+    for (ll i = 1; i <= x; i++)
     {
-        cout << -1;
+        for (ll j = 0; j < n; j++)
+        {
+            if (i - coins[j] >= 0)
+            {
+                dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+            }
+        }
+    }
+
+    if (dp[x] == 1e9)
+    {
+        cout << -1 << endl;
         return 0;
     }
-
-    cout << recom(n, x, coins);
+    cout << dp[x] << endl;
 
     return 0;
 }
