82f2b39c3b60abc091dbe135d1fc150512b7a366
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-04-08 19:31:19 +0530

    Random CF Rated 1000 : Needed to find a condition for BOB to win, and I was able to find one but was unable to find the other...Needed to see the tutorial for that.

---
+++ Random CF Rated 1000/C007_RemovalsGame.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vll a(n), b(n);
+
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+
+        if (a == b)
+        {
+            cout << "Bob\n";
+            continue;
+        }
+
+        reverse(a.begin(), a.end());
+        if (a == b)
+        {
+            cout << "Bob\n";
+            continue;
+        }
+
+        cout << "Alice\n";
+    }
+
+    return 0;
+}
