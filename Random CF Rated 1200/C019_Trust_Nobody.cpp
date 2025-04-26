f103f192928beefc7ec7e763d1a9cd3afd958e44
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-04-26 12:33:20 +0530

    Random CF Rated 1200 : Tried it what I thought was no where near the solution. Looked up the tutorial and then understood what to do. Needed to check for each value starting from zero and brute force and find the answer.

---
+++ Random CF Rated 1200/C019_Trust_Nobody.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+void take_input_vector(vll &nums, ll n)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+}
+
+void print_vector_to_debug(vll &nums, ll n)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vll nums(n);
+        take_input_vector(nums, n);
+
+        bool flag = 0;
+        for (ll j = 0; j <= n; j++)
+        {
+            ll liars = 0;
+            for (auto i : nums)
+            {
+                if (!(j >= i))
+                {
+                    liars++;
+                }
+            }
+            if (liars == j)
+            {
+                cout << j << '\n';
+                flag = 1;
+            }
+            if (flag)
+                break;
+        }
+        if (flag)
+        {
+            continue;
+        }
+        cout << "-1\n";
+    }
+
+    return 0;
+}
