f6a9a71d567a4e1381201df25399d87708f0369d
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-07-16 07:56:04 +0530

    CF Rated 1300 : C006 Strong Vertices : Did not have any clue at all how to solve it in O(n). Read the tutorial and it was like 30 sec solution. Should have played with testcases a bit.

---
+++ CF Rated 1300/C006_Strong_Vertices.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+void take_input_vector(vll &nums, ll n)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+}
+
+void print_vector_to_debug(vll &nums, ll n)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vll a(n), b(n);
+        take_input_vector(a, n);
+        take_input_vector(b, n);
+
+        // print_vector_to_debug(a, n);
+        // print_vector_to_debug(b, n);
+
+        vll c(n);
+        for (int i = 0; i < n; i++)
+        {
+            c[i] = a[i] - b[i];
+        }
+
+        ll maxi = *max_element(c.begin(), c.end());
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            if (maxi == c[i])
+            {
+                ans.push_back(i + 1);
+            }
+        }
+
+        cout << ans.size() << endl;
+
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
