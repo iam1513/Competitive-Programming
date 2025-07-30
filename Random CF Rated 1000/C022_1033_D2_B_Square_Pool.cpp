9c29bf90b8c997bbd671968213f5b2ce5cd9e5b9
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-07-30 09:08:05 +0530

    Random CF : Rated 1000 : Intuition was on point but was not able to figure out how to code it easily.

---
+++ Random CF Rated 1000/C022_1033_D2_B_Square_Pool.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define pb push_back
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+void take_input_vector(vll &nums, ll n)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+}
+
+void print_vector_to_debug(vll &nums, ll n)
+{
+    for (ll i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, s;
+        cin >> n >> s;
+
+        ll sum = 0;
+
+        vector<pair<ll, ll>> pairs;
+
+        ll j = n, i = 0;
+
+        while (n--)
+        {
+            ll dx, dy, x, y;
+            cin >> dx >> dy >> x >> y;
+
+            if (dx == dy)
+            {
+                sum += (x == y);
+            }
+            else if (x + y == s)
+            {
+                sum++;
+            }
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
